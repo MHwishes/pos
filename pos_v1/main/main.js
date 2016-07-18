25fb817e403cdee7f6728182c65138b36f3ab939
mahong <1025383143@qq.com>
2016-07-18 22:15:09 +0800

    second

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,25 +1,23 @@
 'use strict';
-let buildCartItems=(inputs, allItems)=> {
+let buildCartItems = (inputs, allItems)=> {
   const cartItems = [];
-
   for (let input of inputs) {
     const splittedInput = input.split('-');
-    var barcode = splittedInput[0];
-    var count = parseFloat(splittedInput[1] || 1);
+    const barcode = splittedInput[0];
+    const count = parseFloat(splittedInput[1] || 1);
+
     const cartItem = cartItems.find((cartItem)=> {
       return cartItem.item.barcode === barcode;
     });
-
     if (cartItem) {
       cartItem.count++;
     }
     else {
-      const item = allItems.find((allItem)=> {
-        return allItem.barcode === barcode;
+      const item=allItems.find((allItem)=>{
+        return allItem.barcode===barcode;
       });
-      cartItems.push({item: item, count: count});
+      cartItems.push({item:item,count:count});
     }
   }
-
   return cartItems;
 }
