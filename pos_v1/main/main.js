7bc6dbc764217211a08e064b03db8903fb88c101
mahong <1025383143@qq.com>
2016-07-18 21:52:46 +0800

    second

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,44 +1,25 @@
 'use strict';
+let buildCartItems=(inputs, allItems)=> {
+  const cartItems = [];
 
-function printReceipt(inputs) {
-  const allItems = loadAllItems();
-  const cartItems = buildCartItems(inputs, allItems);
-}
+  for (let input of inputs) {
+    const splittedInput = input.split('-');
+    var barcode = splittedInput[0];
+    var count = parseFloat(splittedInput[1] || 1);
+    const cartItem = cartItems.find((cartItem)=> {
+      return cartItem.item.barcode === barcode;
+    });
 
-
-function findCartItems(barcodeArray, allItems) {
-  for (let i = 0; i < allItems.length; i++) {
-    if (barcodeArray === allItems[i].barcode) {
-      return allItems[i];
-    }
-  }
-}
-
-function findExistCartItems(input, cartItems) {
-  for (let i = 0; i < cartItems.length; i++) {
-    if (input === cartItems[i].item.barcode) {
-      return cartItems[i];
-    }
-  }
-  
-  return false;
-}
-
-function buildCartItems(inputs, allItems) {
-  var cartItems = [];
-  inputs.forEach(function (input) {
-    var barcodeArray = input.split('-');
-    var countItem = parseFloat(barcodeArray[1] || 1);
-
-    var item = findCartItems(barcodeArray[0], allItems);
-    var cartItem = findExistCartItems(input, cartItems);
     if (cartItem) {
       cartItem.count++;
     }
     else {
-      cartItems.push({item: item, count: countItem});
+      const item = allItems.find((allItem)=> {
+        return allItem.barcode === barcode;
+      });
+      cartItems.push({item: item, count: count});
     }
-  });
+  }
 
   return cartItems;
 }
